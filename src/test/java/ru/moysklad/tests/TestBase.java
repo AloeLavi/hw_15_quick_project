86ab9d9330efc758bac75245589392b96d0a0fc5
AloeLavi <story_teller@bk.ru>
2022-11-19 14:47:04 +0300

    mvp2 for jenkins

--- src/test/java/ru/moysklad/tests/TestBase.java
+++ src/test/java/ru/moysklad/tests/TestBase.java
@@ -1,8 +1,11 @@
 package ru.moysklad.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -11,8 +14,17 @@ public class TestBase {
 
     @BeforeAll
     static void configure() {
+
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+
         Configuration.baseUrl = "https://online.moysklad.ru";
         Configuration.browserSize = "1440x1080";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
     }
 
     @BeforeEach
