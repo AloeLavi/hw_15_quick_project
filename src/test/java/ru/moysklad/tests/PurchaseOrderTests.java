67ced0b7c5903e4fd29b3ffed15d82d0d3d57a11
AloeLavi <story_teller@bk.ru>
2022-11-19 15:03:58 +0300

    addes steps to Purchase

--- src/test/java/ru/moysklad/tests/PurchaseOrderTests.java
+++ src/test/java/ru/moysklad/tests/PurchaseOrderTests.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import ru.moysklad.pages.*;
 
 import static com.codeborne.selenide.Selenide.sleep;
+import static io.qameta.allure.Allure.step;
 
 public class PurchaseOrderTests extends TestBase {
     PurchaseOrderList purchaseOrderList = new PurchaseOrderList();
@@ -16,14 +17,24 @@ public class PurchaseOrderTests extends TestBase {
     @Test
     @DisplayName("Создание заказа поставщику с заполненными обязательными полями")
     void createPurchaseOrderWithRequiredFields(){
-              purchaseOrderList.openList()
-                .openNew();
-      purchaseOrderPage.setCounterparty("ООО \"Поставщик\"")
-                .saveDocument()
-                .checkDialogMiddleCenter("Заказ создан")
-                .closeDocument();
-      sleep(2000);
-        purchaseOrderList.checkDocumentExistanceByNumber("00001");
+        step("Открыть реестр Заказов покупателей", () -> {
+                    purchaseOrderList.openList();
+                });
+        step("Нажать +Заказ", () -> {
+            purchaseOrderList.openNew();
+        });
+        step("Заполнить поставщика", () -> {
+            purchaseOrderPage.setCounterparty("ООО \"Поставщик\"");
+        });
+        step("Cохранить и закрыть документ", () -> {
+            purchaseOrderPage.saveDocument()
+                    .checkDialogMiddleCenter("Заказ создан")
+                    .closeDocument();
+            sleep(2000);
+        });
+        step("Выйти в реестр и проверить наличие Заказа поставщику 00001", () -> {
+            purchaseOrderList.checkDocumentExistanceByNumber("00001");
+        });
 
     }
 
